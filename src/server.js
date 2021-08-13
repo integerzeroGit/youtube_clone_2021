cbcd96858921096b1a2fcc27f9e70ce55f06bc2e
integerzeroGit <favoritemail.integerzero@gmail.com>
2021-08-13 15:51:42 +0900

    create server
    create a node server

---
+++ src/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () => {
+    console.log(`Server listening on port http://localhost:${PORT}`);
+}
+
+app.listen(PORT, handleListening);
